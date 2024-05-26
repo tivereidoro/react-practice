dcdc0806d0564bd12d26a6ee27b39439943d2b27
tivereidoro <tivereidoro@yahoo.com>
2024-05-26 18:25:35 +0100

    Making progress

---
+++ n-rxt/src/components/App.jsx
@@ -0,0 +1,14 @@
+// import './App.css'
+import Footer from "./footer/Footer"
+import Header from "./header/Header"
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <h1>Hello there.. The body here</h1>
+
+      <Footer />
+    </>
+  )
+}
---
+++ n-rxt/src/components/header/Header.jsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+    return (
+        <div>
+            <h3>This is the header</h3>
+        </div>
+    )
+}
